46ed28957777077de46fb09a72aae3416c77946f
Tianhao <zhangth1990@hotmail.com>
2018-04-14 23:58:15 -0600

    added log

--- app/src/main/java/com/example/tianhao/menudemo/MainActivity.java
+++ app/src/main/java/com/example/tianhao/menudemo/MainActivity.java
@@ -2,17 +2,35 @@ package com.example.tianhao.menudemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.main_menu,menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()){
+            case R.id.settings:
+            Log.i("Item Selected", "Setting");
+            return true;
+            case R.id.help:
+                Log.i("Item Selected", "Help");
+            default:
+                return false;
+        }
     }
 
     @Override
